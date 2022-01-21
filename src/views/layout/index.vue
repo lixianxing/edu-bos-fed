2ace6616894bde4b7df49e306a84590c151e6ca7
lixianxing <63960045+lixianxing@users.noreply.github.com>
2022-01-21 17:58:18 +0800

    路由配置

---
+++ src/views/layout/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout">
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ src/views/role/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="role">角色</div>
+</template>
+<script>
+export default {
+  name: 'Role'
+}
+</script>
+<style lang="scss" scoped></style>
